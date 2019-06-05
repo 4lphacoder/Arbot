ee96a9c853f9ef529566889b792d19c190ec296a
Preeti Yadav <preeti09011999@gmail.com>
2019-06-05 15:04:11 +0530

    added funnctionality of creating a csv file

--- arbot.py
+++ arbot.py
@@ -39,7 +39,8 @@ def initialising_selenium():
 			[ SOLUTION ] Download Chrome browser & chrome webdrivers and make the path of chrome drivers Global Path variable...
 			''')
 		sys.exit()
-	driver.delete_all_cookies()
+	# Uncomment for deleting cookies
+	# driver.delete_all_cookies() 
 	return driver
 
 def banner():
@@ -105,6 +106,19 @@ def update_global_variables(optlist,args):
 		if optlist[i][0] == '-n':
 			NUMBER = int(optlist[i][1])
 
+def login(driver):
+	global USERID
+	global PASSWD
+	print('[ + ] Requesting login page')
+	driver.get('https://www.trademe.co.nz/Members/Login.aspx?url=%2fdefault.aspx')
+	print('[ + ] Login page recieved')
+	print('[ + ] Entering credentials')
+	driver.find_element_by_xpath('//*[@id="page_email"]').send_keys(USERID)
+	driver.find_element_by_xpath('//*[@id="page_password"]').send_keys(PASSWD)
+	input('[ + ] Captcha found, press return after filling it')
+	print('[ + ] Logging In')
+	driver.find_element_by_xpath('//*[@id="LoginPageButton"]').click()
+
 
 if __name__ == "__main__":
 	banner()
@@ -115,7 +129,7 @@ if __name__ == "__main__":
 	print('[ + ] Web Browser loaded')
 
 	print('[ + ] Opening Webpage')
-	driver.get('https://www.trademe.co.nz/')
+	login(driver)   # login redirects to home page
 	print('[ + ] Webpage Loaded Completely')
 	wait = WebDriverWait(driver, 20)
 	search_field = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="searchString"]')))
@@ -134,9 +148,59 @@ if __name__ == "__main__":
 	print('[ + ] Bundling list of clients')
 
 	print('[ + ] Creating list of client')
-	client_list = client_bundle.find_elements_by_tag_name('a')
-	
+	client_list = client_bundle.find_elements_by_class_name('listingTitle')	
 	for i in range(len(client_list)):
-		print(client_list[i].text)
+		client_list[i] = client_list[i].find_element_by_tag_name('a').get_attribute('href')
+	
+	print('[ + ] Storing clients address in file client_addr.txt')
+	fd = open('client_addr.txt','w')
+	for client_addr in client_list:
+		fd.write(client_addr)
+		fd.write('\n')
+	fd.close()
 
+	print('[ + ] Extraction started')
+	fd = open(str('scraping_on_keyword_'+KEYWORD+'.csv'),'w')
+	client_name = ''
+	client_num1 = ''
+	client_num2 = ''
+	client_location = ''
+	business_name = ''
+	about = ''
+	for client_addr in client_list:
+		print('\n[ + ] Extracting from '+client_addr)
+		driver.get(client_addr)
+		print('[ + ] Client\'s page loaded')
+		try:
+			client_name = driver.find_element_by_xpath('//*[@id="ClassifiedActions_ServiceContactName"]').text.replace('\n',', ')
+		except:
+			pass
+		try:
+			client_num1 = driver.find_element_by_xpath('//*[@id="ClassifiedActions_ServicePhone1"]').text
+		except:
+			pass
+		try:
+			client_num2 = driver.find_element_by_xpath('//*[@id="ClassifiedActions_ServicePhone2"]').text
+		except:
+			pass
+		try:
+			client_location = driver.find_element_by_xpath('//*[@id="ClassifiedActions_ServiceLocation"]').text.replace('\n',', ')
+		except:
+			pass
+		try:
+			business_name = driver.find_element_by_xpath('//*[@id="ListingAttributes"]/tbody/tr[1]/td').text.replace('\n',', ')
+		except:
+			pass
+		try:
+			about = driver.find_element_by_xpath('//*[@id="ListingAttributes"]/tbody/tr[3]/td').text.replace('\n',',')
+		except:
+			pass
+
+		print('[ + ] Extraction from '+client_addr+' completed')
+		print('Name : '+client_name+'\nBusiness Name : '+business_name+'\nPhone : '+client_num1+' , '+client_num2+'\nClient\'s Location : '+client_location+'\nAbout : '+about+'\n')
+		fd.write(client_name+';'+business_name+';'+client_num1+';'+client_num2+';'+KEYWORD+';'+client_location+';'+about+'\n')
+		fd.flush()
+		input('[ + ] Press return to continue')
+		
+	
 	#wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="ListViewList"]')))
